bd82a233a26bdc9adfe9fa67a60d4512ac4172c0
Camilo Ramirez <juancamilor26@hotmail.com>
2023-03-21 21:53:56 -0500

    Prueba commit

---
+++ src/main/java/com/tutorial/bikeservice/controller/BikeController.java
@@ -0,0 +1,50 @@
+package com.tutorial.bikeservice.controller;
+
+import com.tutorial.bikeservice.entity.Bike;
+import com.tutorial.bikeservice.service.BikeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bike")
+public class BikeController {
+
+    @Autowired
+    BikeService bikeService;
+
+    @GetMapping
+    public ResponseEntity<List<Bike>> getAll(){
+        List<Bike> bikes = bikeService.getAll();
+        if (bikes.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }else {
+            return ResponseEntity.ok(bikes);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Bike> findById(@PathVariable("id") int id){
+        Bike bikes = bikeService.getBikeById(id);
+        if (bikes == null){
+            return ResponseEntity.notFound().build();
+        }else {
+            return ResponseEntity.ok(bikes);
+        }
+    }
+
+    @PostMapping()
+    public ResponseEntity<Bike> save(@RequestBody Bike bike ){
+        Bike bikeNew = bikeService.save(bike);
+            return ResponseEntity.ok(bikeNew);
+    }
+
+    @GetMapping("/byuser/{userId}")
+    public ResponseEntity<List<Bike>> getByUserId(@PathVariable("userId") int userId){
+        List<Bike> bikes = bikeService.byUserId(userId);
+        return ResponseEntity.ok(bikes);
+    }
+
+}
