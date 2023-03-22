6dd011fb518bb608e27e925ab09e444517fcc180
Camilo Ramirez <juancamilor26@hotmail.com>
2023-03-21 19:38:10 -0500

    first commit

---
+++ user-service/src/main/java/com/tutorial/userservice/entity/User.java
@@ -0,0 +1,22 @@
+package com.tutorial.userservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String email;
+
+}
